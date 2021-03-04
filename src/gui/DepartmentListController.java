c76646bf52acd69707766b9a2df75502941fd2de
jminango20 <jcarlosminango@gmail.com>
2021-03-04 11:01:14 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	//Uso classe service
+	private DepartmentService service;
 
 	@FXML
 	private TableView<Department> tableViewDepartment;
@@ -27,9 +34,19 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	
+	private ObservableList<Department> obsList;
+	
+	
+	
 	@FXML
 	public void onBtNewAction() { //Button Department
 		System.out.println("onBtNewAction");
+	}
+	
+	//Injetar: dependencia inicializacao no DepartmentService
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	
@@ -46,6 +63,15 @@ public class DepartmentListController implements Initializable {
 		//tableView department acompanhe o tamanho da janela
 		Stage stage = (Stage) Main.getMainScene().getWindow(); //Windonws eh superclase de stage -> Downcasting
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll(); //pega a lista da clase DepartmentService
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
 	}
 
 }
